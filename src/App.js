f2aa585c62e1d18e074918a26637d3bf646fb921
OSUpdate <force185@naver.com>
2019-11-06 19:23:41 +0900

    html update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import styles from './App.css';
+import React from "react";
+import styles from "./App.css";
 
 function App() {
   return (
     <div className={styles.App}>
-      <header className={styles.App_header}>
-        <img src={logo} className={styles.App_logo} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.App_link}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header id={styles.banner}>
       </header>
+      <div className={styles.titlebar}>
+
+        <a href="#" className={styles.toggle + " fas"}></a>
+        <a href="#">blog</a>
+      </div>
+      <section className={styles.wrapper}>
+        <div className={styles.sidebar}>
+          <div className={styles.inner}>
+            <div className={styles.search}>
+              <form className={"fas"}>
+                <input type="text" name="query" id="query" placeholder="Search"/>
+              </form>
+            </div>
+            <nav className={styles.menu}>
+              <header>
+                <h3>Menu</h3>
+              </header>
+              <ul>
+                <li><a href="#">Menu</a></li>
+                <li><span className={styles.opener + " fas fa-chevron-down"}>Sub Menu</span></li>
+                <li><a href="#">Menu</a></li>
+                <li><a href="#">Menu</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        
+        <section className={styles.contents}>
+          <div className={styles.contents_inner}>
+            
+            <div className={styles.title}>
+              <h2>title</h2>
+            </div>
+            <div className={styles.post}>
+              <h3>post title</h3>
+              <div className={styles.content}>
+                <p>content</p>
+              </div>
+            </div>
+            <div className={styles.post}>
+              <h3>post title</h3>
+              <div className={styles.content}>
+                <p>content</p>
+              </div>
+            </div>
+            <div className={styles.post}>
+              <h3>post title</h3>
+              <div className={styles.content}>
+                <p>content</p>
+              </div>
+            </div>
+            <div className={styles.post}>
+              <h3>post title</h3>
+              <div className={styles.content}>
+                <p>content</p>
+              </div>
+            </div>
+            <div className={styles.post}>
+              <h3>post title</h3>
+              <div className={styles.content}>
+                <p>content</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.slide}>
+            <span className={"fas fa-chevron-left "+styles.left}></span>
+            <span className={"fas fa-chevron-right "+styles.right}></span>
+          </div>
+        </section>
+      </section>
+
     </div>
   );
 }
